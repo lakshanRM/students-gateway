bc519fd94a56ad469105a8570873b8d10c3f30b4
LakshanRM <lakshanM@fortude.co>
2021-09-21 18:01:58 +0530

    Save with file names

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -5,15 +5,27 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express/multer';
+import { diskStorage } from 'multer';
+import { editFileName, fileFilter } from './file-upload.utils';
 
 @Controller('uploads')
 export class UploadsController {
   @Post('uploadFile')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return {
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './upload',
+        filename: editFileName,
+      }),
+      fileFilter: fileFilter,
+    }),
+  )
+  async uploadFile(@UploadedFile() file) {
+    const response = {
       status: 'success',
-      data: file.filename,
+      originalname: file.originalname,
+      filename: file.filename,
     };
+    return response;
   }
 }
